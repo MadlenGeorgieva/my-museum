f650335697eb9e30313eaec89a6165ffbed7570e
MadlenGeorgieva <madlengeorgieva2005@gmail.com>
2026-04-09 09:05:16 +0200

    v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-
-import './App.css'
-
-export default function App() {
-  
-  
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ExhibitionList from "./pages/ExhibitionList";
+function App() {
+return (
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<HomePage />} />
+<Route path="/exhibitions" element={<ExhibitionList />} />
+{/* Add more routes as you build more pages */}
+</Routes>
+</BrowserRouter>
+);
 }
+export default App
