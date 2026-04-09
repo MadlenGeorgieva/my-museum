3284776fb8fbda99f287929c36737c9cf1b61586
MadlenGeorgieva <madlengeorgieva2005@gmail.com>
2026-04-09 11:10:32 +0200

    v23

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,15 @@ import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer>
-        <div>
-            <p>Adress: 123 Botanical Lane, Green City, 2345 Copenhagen, Denmark</p>
-            <p>Email: flowplant123@flowerplant2026.dk</p>
-            <p>Phone: +452076765</p>
-            <p>© 2026 FlowerPlant. All rights reserved</p>
+    <footer className={styles.footer}>
+      <div className={styles.inner}>
+        <div className={styles.contact}>
+          <p>Address: 123 Botanical Lane, Green City, 2345 Copenhagen, Denmark</p>
+          <p>Email: flowplant123@flowerplant2026.dk</p>
+          <p>Phone: +45 20 76 76 65</p>
         </div>
+        <p className={styles.copy}>© 2026 FlowerPlant. All rights reserved.</p>
+      </div>
     </footer>
   );
 }
